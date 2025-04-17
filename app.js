8d4505065f5ef883e7c515bf5370b9092e9e3aa3
Pegwende Zabsore <pegwende.zabsore@workday.com>
2025-04-16 23:28:02 -0400

    added console to check

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const db = await mysql.createConnection({
     host: process.env.DB_HOST,
@@ -22,6 +22,8 @@ const db = await mysql.createConnection({
     queueLimit: 0,
 })
 console.log("Connected to MySQL database");
+console.log("Connecting to DB:", process.env.DB_HOST);
+
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 

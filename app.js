2d3bbd53df5e756bfe8d91b449068e915ba8acb8
Pegwende Zabsore <pegwende.zabsore@workday.com>
2025-04-16 22:25:15 -0400

    Commit to deploy

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 
 const db = await mysql.createConnection({
     host: process.env.DB_HOST,
